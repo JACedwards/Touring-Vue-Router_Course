9e2d24da3f957bb8613d72a578e3348bfe778655
Craig Edwards <jamesallyncraigedwards@gmail.com>
2022-07-24 10:32:27 -0400

    l7_mine

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,20 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Regiser Me</button>
 </template>
 
 <script>
+
+
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+        // Cal to applicationIf registered then redirect ot even details
+        this.$router.push({
+            name: 'EventDetails'
+        })
+    }
+  }
 }
 </script>
